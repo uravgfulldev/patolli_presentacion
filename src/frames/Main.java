0dcd580aafae6f6577267683891953c6f9f1fa99
unknown <Hiram610>
2022-12-06 01:34:08 -0700

    Hola

--- src/frames/Main.java
+++ src/frames/Main.java
@@ -14,6 +14,9 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        System.out.println("Hola");
+        
         FrmInicio frmInicio=new FrmInicio();
         frmInicio.setVisible(true);
     }
