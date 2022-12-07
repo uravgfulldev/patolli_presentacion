5f1bf3ae350c682519a0576a6648b87a6707769f
Hugo Rivera <hriveravz@gmail.com>
2022-12-07 11:21:09 -0700

    Se agrego al dominio IServidor y se hizo serializable la partida

--- src/frames/Main.java
+++ src/frames/Main.java
@@ -15,8 +15,6 @@ public class Main {
      */
     public static void main(String[] args) {
         
-        System.out.println("Hola");
-        
         FrmInicio frmInicio=new FrmInicio();
         frmInicio.setVisible(true);
     }
